74577e6e069f1b6c1088eba6bd2b0ed59c813116
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-06-07 21:07:10 -0300

    maluquices ocorreram

--- Projeto.py
+++ Projeto.py
@@ -1,16 +1,16 @@
 import time
-
+from imblearn.over_sampling import SMOTE
 import graphviz
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn import metrics
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-
-
+import plotly.express as px
 
 
 def renomear(df):
@@ -25,106 +25,122 @@ def renomear(df):
     df.rename(columns=new_column_names, inplace=True)
     return df
 
-def holdout(df, test_size):
+def holdout(df, test_size, arvore, vizinhos, f_names):
     X = df.iloc[:, 1:].values
     y = df.iloc[:, 0].values
 
     # Dividindo os dados em treino e teste (80% treino, 20% teste)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size,shuffle=True)
 
 
-    treinamento = pd.DataFrame(X_train)
-    teste = pd.DataFrame(X_test)
 
-    teste = renomear(teste)
-    treinamento = renomear(treinamento)
-    # Obter os nomes das colunas (exceto a primeira, que é a etiqueta)
-    feature_names = df.columns[1:]
-    count0 = 0
-    count2 =0
-    for valor in y_train:
-        if valor == 0.0:
-            count0 += 1
-        if valor == 2.0:
-            count2 +=1
-
+    count0, count1, count2 = sum(y_train == 0), sum(y_train == 1), sum(y_train == 2)
     print(count0)
+    print(count1)
     print(count2)
-
-    print("Accuracy")
-    print("Arvore: ", arvore_decisao(treinamento, y_train, feature_names, teste, y_test))
-    print("")
-    print("KNN: ",KNN(treinamento, y_train, 2, teste, y_test))
+    print("----")
+    # Determinar o fator de aumento para cada classe
 
 
-def r_fold_cross_validation(df, r_folds):
+    smote = SMOTE(sampling_strategy='not majority',random_state=65478798)
+    X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+    count0, count1, count2 = sum(y_train_resampled == 0), sum(y_train_resampled == 1), sum(y_train_resampled == 2)
+    print(count0)
+    print(count1)
+    print(count2)
+    treinamento = pd.DataFrame(X_train_resampled)
+
+
+
+    treinamento = renomear(treinamento)
+
+    smoteteste = SMOTE(sampling_strategy='not majority', random_state=44787)
+    X_test_resampled, y_test_resampled = smoteteste.fit_resample(X_test, y_test)
+    teste = pd.DataFrame(X_test_resampled)
+    teste = renomear(teste)
+    print("Holdout")
+    print("Arvore")
+    arvore.fit(treinamento, y_train_resampled)
+    arquivo = 'tree_modelov1.dot'
+
+    export_graphviz(arvore, out_file=arquivo, feature_names=f_names, class_names=True)
+    with open(arquivo) as f:
+        dot_graph = f.read()
+    grafico = graphviz.Source(dot_graph)
+    grafico.render(view=True)
+
+    y_pred = arvore.predict(teste)
+    print(metrics.confusion_matrix(y_test_resampled, y_pred))
+    print("Acuracia:", metrics.accuracy_score(y_test_resampled, y_pred))
+    print("Precisao: ", metrics.precision_score(y_test_resampled, y_pred, average=None))
+    print("KNN: ")
+    vizinhos.fit(treinamento, y_train_resampled)
+    y_pred = vizinhos.predict(teste)
+    print(metrics.confusion_matrix(y_test_resampled, y_pred))
+    print("Acuracia: ", metrics.accuracy_score(y_test_resampled, y_pred))
+    print("Precisao: ", metrics.precision_score(y_test_resampled,y_pred, average=None))
+
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(X_test_resampled[:, 0], X_test_resampled[:, 1], c=y_pred, cmap='viridis')
+    plt.title('KNN - Classificação das Amostras')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.colorbar(label='Classe Prevista')
+    plt.show()
+
+
+def r_fold_cross_validation(df, r_folds,modelo, modeloNome):
     X = df.iloc[:, 1:].values
     y = df.iloc[:, 0].values
-
-    kfold = KFold(r_folds, shuffle=True, random_state=None)
-    accuracies_tree = []
-    accuracies_KNN = []
-    for i, (train_index, test_index) in enumerate(kfold.split(X)):
-        '''print(f"Fold {i}:")
-        print(f"  Train: index={train_index}")
-        print(f"  Test:  index={test_index}")'''
-
+    acc = []
+    prec = []
+    kf = StratifiedKFold(n_splits=r_folds, shuffle=True, random_state=42)
+    for i, (train_index, test_index) in enumerate(kf.split(X, y)):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        treinamento = pd.DataFrame(X_train)
+        smote = SMOTE(sampling_strategy='minority',random_state=42)
+        X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+        treinamento = pd.DataFrame(X_train_resampled)
         teste = pd.DataFrame(X_test)
 
         teste = renomear(teste)
         treinamento = renomear(treinamento)
-        # Obter os nomes das colunas (exceto a primeira, que é a etiqueta)
-        feature_names = df.columns[1:]
-        accuracies_tree.append(arvore_decisao(treinamento, y_train, feature_names, teste, y_test))
-        accuracies_KNN.append(KNN(treinamento, y_train, 15, teste, y_test))
-    print("Accuracy")
-    print("Arvore: ", np.mean(accuracies_tree))
-    #print("KNN: ", np.mean(accuracies_KNN))
+
+        modelo.fit(treinamento, y_train_resampled)
+        y_pred = modelo.predict(teste)
+        print(metrics.confusion_matrix(y_test, y_pred))
+        acc.append(metrics.accuracy_score(y_test, y_pred))
+        prec.append(metrics.precision_score(y_test,y_pred, average=None))
+    mediAcc = np.mean(acc)
+    mediaPrec_classes = np.mean(prec, axis=0)
+    print("Acuracia: ", mediAcc)
+    print("Precisao: ", mediaPrec_classes)
 
 
-def arvore_decisao(treinamento, y_train, f_names,x_teste, y_teste):
-    arvore_decisao = DecisionTreeClassifier(max_depth=None,
-                                            max_features=None,
+def arvore_decisao(profundidade, minLeaf, minSample):
+    arvore_decisao = DecisionTreeClassifier(max_depth=profundidade,
+                                            max_features=10,
                                             criterion="entropy",
-                                            min_samples_leaf=1,
-                                            min_samples_split=2)
-    arvore_decisao.fit(treinamento, y_train)
-    '''arquivo = 'tree_modelov1.dot'
-
-    export_graphviz(arvore_decisao, out_file=arquivo, feature_names=f_names, class_names=True)
-    with open(arquivo) as f:
-        dot_graph = f.read()
-    grafico = graphviz.Source(dot_graph)
-    grafico.render(view=True)'''
-
-    #print(arvore_decisao.get_depth())
-    #print(arvore_decisao.get_n_leaves())
-    y_pred = arvore_decisao.predict(x_teste)
-
-
-    #print(accuracy_score(y_teste,y_pred))
-    #print(arvore_decisao.score(x_teste, y_teste, None))
+                                            min_samples_leaf=minLeaf,
+                                            min_samples_split=minSample)
 
 
 
-    print(metrics.confusion_matrix(y_teste, y_pred))
-    return accuracy_score(y_teste,y_pred)
 
-def KNN(treinamento,y_train,K, x_teste, y_teste):
+    return arvore_decisao
+
+def KNN(K):
     k_vizinhos = KNeighborsClassifier(n_neighbors=K,
                                       weights='uniform',
                                       algorithm='auto',
                                       p=2,
                                       metric_params=None,
                                       n_jobs=None)
-    k_vizinhos.fit(treinamento, y_train)
 
-    y_pred = k_vizinhos.predict(x_teste)
-    print(metrics.confusion_matrix(y_teste, y_pred))
-    return accuracy_score(y_teste, y_pred)
+    return k_vizinhos
 
 def main():
     inicio = time.time()
@@ -133,14 +149,14 @@ def main():
     #primeira_feature = df[df.iloc[:, 0] == 0]
 
     # Selecionar metade desses registros de forma aleatória
-    #metade_para_remover = primeira_feature.sample(frac=0, random_state=1)
+    #metade_para_remover = primeira_feature.sample(frac=0.5, random_state=1)
 
     # Remover esses registros do DataFrame original
     #df = df.drop(metade_para_remover.index)
 
-    #primeira_feature = df[df.iloc[:, 0] == 1]
-    #metade_para_remover = primeira_feature.sample(frac=0, random_state=1)
-    #df = df.drop(metade_para_remover.index)
+    primeira_feature = df[df.iloc[:, 0] == 1]
+    metade_para_remover = primeira_feature.sample(frac=0, random_state=1)
+    df = df.drop(metade_para_remover.index)
 
     '''
     print(f"Número de linhas de treinamento: {len(treinamento)}")
@@ -148,9 +164,13 @@ def main():
     #print(df)
     #print(treinamento)
     #print(teste)
-
-    holdout(df,0.2)
-    #r_fold_cross_validation(df,2)
+    feature_names = df.columns[1:]
+    AD = arvore_decisao(10,1,2)
+    vizinhos = KNN(100)
+    df.sample()
+    holdout(df,0.2, AD, vizinhos, feature_names)
+    #r_fold_cross_validation(df,3, AD, 'Arvore: ')
+    #r_fold_cross_validation(df, 3, vizinhos, 'KNN: ')
     fim = time.time()
     print(f"{fim - inicio:.2f} s")
 
